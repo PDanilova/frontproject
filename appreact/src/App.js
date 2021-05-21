339c3740917580f5e0b8c0247128558119224d6e
Polina Danilova <p.danilova@greendatasoft.ru>
2021-05-21 21:40:52 +0500

    first commit

---
+++ appreact/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1 style={{ padding: "10px", textAlign: "center", color: "lightblue", fontSize: 150 }}>
+      Hello, world!
+      </h1>
+      
+    );
+  }
+}
+
+export default App;

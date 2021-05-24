96d480a6f762f26073bf0f9ed676a754e14f85ad
Polina Danilova <p.danilova@greendatasoft.ru>
2021-05-24 11:07:18 +0500

    appreact typescrypt version. first commit

---
+++ appreact_ts/src/App.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const App: React.FunctionComponent = () => {
+  return <h1 style={{ textAlign: "center", color: "lightgreen", fontSize: 150 }}>
+    Hello, world!
+  </h1>
+}
+
+export default App;
